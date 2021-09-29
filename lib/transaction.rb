c77d11f2f9b5a04ca28f1a5cf6f843281a0bbf97
Corinne <corinne.boesch@gmail.com>
2021-09-29 21:05:57 +0100

    covering edge cases for withdrawal amount input

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,27 +2,34 @@ require 'date'
 
 class Transaction
 
+  ERROR1 = 'Please deposit a valid amount above 5 pounds'
+  ERROR2 = 'Only deposits in pounds with no pence acceppted'
+  ERROR3 = 'Deposit amount needs to be above 5 pounds'
+  ERROR4 = 'Please input a positive Number as valid withdraw currecy'
+
   def credit(deposit_amount, balance)
     check_deposit(deposit_amount)
     "#{date_today} || #{'%.2f' % deposit_amount} || || #{'%.2f' % balance}"
   end
 
   def debit(withdrawal_amount, balance)
+    debit_deposit(withdrawal_amount)
     "#{date_today} || || #{'%.2f' % withdrawal_amount} || #{'%.2f' % balance}"
   end
 
   private
   def check_deposit(deposit_amount)
-    error1 = 'Please deposit a valid amount above 5 pounds'
-    raise error1 if deposit_amount.is_a? String
-    error2 = 'Only deposits in pounds with no pence acceppted'
-    raise error2 if deposit_amount.round(0) != deposit_amount
-    raise 'Deposit amount needs to be above 5 pounds' if deposit_amount < 5
+    raise ERROR1 if !deposit_amount.is_a?(Numeric)
+    raise ERROR2 if deposit_amount.round(0) != deposit_amount
+    raise ERROR3 if deposit_amount < 5
+  end
+
+  def debit_deposit(withdrawal_amount)
+    raise ERROR4 if !withdrawal_amount.is_a?(Numeric)
+    raise ERROR4 if withdrawal_amount < 0
   end
 
   def date_today
     Date.today.strftime("%d/%m/%Y")
   end
 end
-
-# acc = Transaction.new.credit('banana', 200)
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,7 +2,7 @@ require 'transaction'
 require_relative 'helper_methods'
 
 describe Transaction do
-  let(:transaction) { described_class.new }
+  let(:transaction) { Transaction.new }
   let(:account) { instance_double('account') }
 
   describe '#credit' do
@@ -15,24 +15,24 @@ describe Transaction do
 
     context 'raises error' do 
       it 'when credit amount is not an integer' do
-        deposit = 'Pineapple'
+        deposit = 'Bitcoin'
         allow(account).to receive(:balance).and_return('200' + deposit)
-        message = 'Please deposit a valid amount above 5 pounds'
-        expect { transaction.credit(deposit, account.balance) }.to raise_error message
+        expect { transaction.credit(deposit, account.balance) }
+        .to raise_error(Transaction::ERROR1)
       end
 
-      it 'when credit amount is decimal values' do
+      it 'when credit amount not a fixnum and has decimal values' do
         deposit = 100.09
         allow(account).to receive(:balance).and_return(200 + deposit)
-        message = 'Only deposits in pounds with no pence acceppted'
-        expect { transaction.credit(deposit, account.balance) }.to raise_error message
+        expect { transaction.credit(deposit, account.balance) }
+        .to raise_error(Transaction::ERROR2)
       end
 
       it 'when credit amount is below 5 pounds' do
         deposit = 4
         allow(account).to receive(:balance).and_return(200 + deposit)
-        message = 'Deposit amount needs to be above 5 pounds'
-        expect { transaction.credit(deposit, account.balance) }.to raise_error message
+        expect { transaction.credit(deposit, account.balance) }
+        .to raise_error(Transaction::ERROR3)
       end
     end
   end
@@ -50,6 +50,22 @@ describe Transaction do
       allow(account).to receive(:balance).and_return(500 - withdrawal)
       transaction_message = "#{test_date} || || 75.76 || 424.24"
       expect(transaction.debit(withdrawal, account.balance)).to eq transaction_message
+    end
+
+    context 'raises error' do 
+      it 'when withdrawal amount is not an integer' do
+        withdrawal = 'Chocolate Gold'
+        allow(account).to receive(:balance).and_return(withdrawal)
+        expect { transaction.debit(withdrawal, account.balance) }
+        .to raise_error(Transaction::ERROR4)
+      end
+
+      it 'when withdrawal amount is negative' do
+        withdrawal = -2.52
+        allow(account).to receive(:balance).and_return(withdrawal)
+        expect { transaction.debit(withdrawal, account.balance) }
+        .to raise_error(Transaction::ERROR4)
+      end
     end
   end
 end
