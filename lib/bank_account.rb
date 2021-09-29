70ecda0efef84c7e024a85ae81da5b3ac38d57ee
Corinne <corinne.boesch@gmail.com>
2021-09-29 11:36:59 +0100

    bank account initializes with name

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,8 @@
 class BankAccount
+  attr_reader :name
+
+  def initialize(account_holders_name)
+   @name = account_holders_name
+  end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,6 +1,11 @@
 require 'bank_account'
 
 describe BankAccount do
-  
-  
+  describe '#initializes' do
+    it 'with a name of the account holder' do 
+      account = BankAccount.new('Sir David Attenborough')
+      expect(account.name).to eq('Sir David Attenborough')
+    end
+  end
+
 end
