1156f18c3f0ccc0772819377ebabcf4ed4811438
Corinne <corinne.boesch@gmail.com>
2021-09-29 14:19:29 +0100

    refactored transactions spec

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,20 +3,23 @@ require_relative 'helper_methods'
 
 describe Transaction do
   let(:transaction) { described_class.new }
+  let(:bank) { instance_double('bank') }
 
   describe '#credit' do
     it 'creates string with date, credit amount, blank debit and total balance' do
       deposit = 100
-      balance = 500
-      expect(transaction.credit(deposit, balance)).to eq "#{test_date} || 100.00 || || 500.00"
+      allow(bank).to receive(:balance).and_return(500 + deposit)
+      transaction_message = "#{test_date} || 100.00 || || 600.00"
+      expect(transaction.credit(deposit, bank.balance)).to eq transaction_message
     end
   end
 
   describe '#debit' do
     it 'creates string with date, blank credit, debit amount and total balance' do
       withdrawal = 100
-      balance = 500
-      expect(transaction.debit(withdrawal, balance)).to eq "#{test_date} || || 100.00 || 500.00"
+      allow(bank).to receive(:balance).and_return(500 - withdrawal)
+      transaction_message = "#{test_date} || || 100.00 || 400.00"
+      expect(transaction.debit(withdrawal, bank.balance)).to eq transaction_message
     end
   end
 end
